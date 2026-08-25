8f66a6d0c7c1f80f0cfe5fd2ecd603793fb927ce
Veselin Totev <v.totev@probg.com>
2026-08-25 19:19:20 +0300

    feat: add English services block language

---
+++ upload/admin/language/en-gb/extension/module/probg_services_block.php
@@ -0,0 +1,5 @@
+<?php
+$_['heading_title']='ProBG Services Block';$_['text_home']='Home';$_['text_extension']='Extensions';$_['text_success']='Module saved successfully.';$_['text_edit']='Block settings';$_['text_enabled']='Enabled';$_['text_disabled']='Disabled';$_['text_latest']='Latest services';$_['text_category']='Services from category';$_['text_featured']='Featured services';
+$_['entry_name']='Module name';$_['entry_title']='Title';$_['entry_mode']='Content type';$_['entry_category']='Category';$_['entry_services']='Selected services';$_['entry_limit']='Limit';$_['entry_width']='Image width';$_['entry_height']='Image height';$_['entry_status']='Status';
+$_['help_services']='Used when the content type is Featured services.';
+$_['error_permission']='You do not have permission to modify this module.';$_['error_name']='Name must be between 3 and 64 characters.';$_['error_limit']='Limit must be between 1 and 100.';
