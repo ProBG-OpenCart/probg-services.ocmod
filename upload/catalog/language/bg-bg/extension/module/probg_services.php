f5fd59e3f664b3c116e43420c1fdfe58458d075c
Veselin Totev <v.totev@probg.com>
2026-08-25 14:44:53 +0300

    feat: add Bulgarian storefront language

---
+++ upload/catalog/language/bg-bg/extension/module/probg_services.php
@@ -0,0 +1,12 @@
+<?php
+$_['heading_title']='Услуги';
+$_['text_home']='Начало';
+$_['text_categories']='Категории услуги';
+$_['text_services']='Услуги';
+$_['text_no_results']='Няма намерени услуги.';
+$_['text_read_more']='Научете повече';
+$_['text_related']='Свързани услуги';
+$_['text_price']='Цена';
+$_['text_not_found']='Страницата не е намерена';
+$_['text_pagination']='Показани %d до %d от %d (%d страници)';
+$_['button_enquiry']='Изпрати запитване';
