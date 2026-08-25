edc50a0c7d75223697eaedb8fbd5ec84a73e3870
Veselin Totev <v.totev@probg.com>
2026-08-25 15:04:41 +0300

    feat: add English enquiry administration language

---
+++ upload/admin/language/en-gb/extension/probg_services/enquiry.php
@@ -0,0 +1,16 @@
+<?php
+$_['heading_title']='Enquiries';
+$_['text_home']='Home';
+$_['text_list']='Enquiry list';
+$_['text_form']='Enquiry #%s';
+$_['text_success']='Enquiry updated successfully.';
+$_['text_success_delete']='Selected enquiries deleted.';
+$_['text_no_results']='No enquiries found.';
+$_['text_pagination']='Showing %d to %d of %d (%d Pages)';
+$_['text_settings']='Settings';$_['text_categories']='Categories';$_['text_services']='Services';$_['text_enquiries']='Enquiries';
+$_['column_id']='ID';$_['column_customer']='Customer';$_['column_email']='Email';$_['column_service']='Service';$_['column_status']='Status';$_['column_date_added']='Date';$_['column_action']='Action';
+$_['entry_filter_name']='Customer';$_['entry_filter_email']='Email';$_['entry_filter_service_id']='Service ID';$_['entry_filter_status']='Status';$_['entry_filter_date_from']='Date from';$_['entry_filter_date_to']='Date to';
+$_['entry_name']='Name';$_['entry_email']='Email';$_['entry_telephone']='Telephone';$_['entry_company']='Company';$_['entry_website']='Website';$_['entry_subject']='Subject';$_['entry_budget']='Budget';$_['entry_deadline']='Desired deadline';$_['entry_message']='Message';$_['entry_service']='Service';$_['entry_store']='Store ID';$_['entry_customer_id']='Customer ID';$_['entry_ip']='IP';$_['entry_user_agent']='User Agent';$_['entry_email_sent']='Email sent';$_['entry_status']='Status';$_['entry_comment']='Internal note';$_['text_files']='Files';$_['text_history']='History';
+$_['button_filter']='Filter';$_['button_delete']='Delete';$_['button_view']='View';$_['button_save']='Save';$_['button_cancel']='Back';
+$_['status_new']='New';$_['status_viewed']='Viewed';$_['status_needs_info']='Needs information';$_['status_quote_sent']='Quote sent';$_['status_in_progress']='In progress';$_['status_accepted']='Accepted';$_['status_rejected']='Rejected';$_['status_completed']='Completed';$_['status_spam']='Spam';
+$_['error_permission']='Warning: You do not have permission to modify enquiries.';
