d4ac8cf1e37661c21c91dc53fe215a3d92c43e8d
Veselin Totev <v.totev@probg.com>
2026-08-25 14:45:03 +0300

    feat: add English storefront language

---
+++ upload/catalog/language/en-gb/extension/module/probg_services.php
@@ -0,0 +1,12 @@
+<?php
+$_['heading_title']='Services';
+$_['text_home']='Home';
+$_['text_categories']='Service Categories';
+$_['text_services']='Services';
+$_['text_no_results']='No services found.';
+$_['text_read_more']='Learn more';
+$_['text_related']='Related services';
+$_['text_price']='Price';
+$_['text_not_found']='Page not found';
+$_['text_pagination']='Showing %d to %d of %d (%d Pages)';
+$_['button_enquiry']='Send enquiry';
