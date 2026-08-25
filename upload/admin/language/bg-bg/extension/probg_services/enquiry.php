a3a477e239ef3194a90597ee9725a6c02df833ed
Veselin Totev <v.totev@probg.com>
2026-08-25 15:04:31 +0300

    feat: add Bulgarian enquiry administration language

---
+++ upload/admin/language/bg-bg/extension/probg_services/enquiry.php
@@ -0,0 +1,16 @@
+<?php
+$_['heading_title']='Запитвания';
+$_['text_home']='Начало';
+$_['text_list']='Списък със запитвания';
+$_['text_form']='Запитване #%s';
+$_['text_success']='Запитването е обновено успешно.';
+$_['text_success_delete']='Избраните запитвания са изтрити.';
+$_['text_no_results']='Няма намерени запитвания.';
+$_['text_pagination']='Показани %d до %d от %d (%d страници)';
+$_['text_settings']='Настройки';$_['text_categories']='Категории';$_['text_services']='Услуги';$_['text_enquiries']='Запитвания';
+$_['column_id']='№';$_['column_customer']='Клиент';$_['column_email']='Имейл';$_['column_service']='Услуга';$_['column_status']='Статус';$_['column_date_added']='Дата';$_['column_action']='Действие';
+$_['entry_filter_name']='Клиент';$_['entry_filter_email']='Имейл';$_['entry_filter_service_id']='ID на услуга';$_['entry_filter_status']='Статус';$_['entry_filter_date_from']='От дата';$_['entry_filter_date_to']='До дата';
+$_['entry_name']='Име';$_['entry_email']='Имейл';$_['entry_telephone']='Телефон';$_['entry_company']='Фирма';$_['entry_website']='Уебсайт';$_['entry_subject']='Тема';$_['entry_budget']='Бюджет';$_['entry_deadline']='Желан срок';$_['entry_message']='Съобщение';$_['entry_service']='Услуга';$_['entry_store']='Store ID';$_['entry_customer_id']='Customer ID';$_['entry_ip']='IP';$_['entry_user_agent']='User Agent';$_['entry_email_sent']='Имейл изпратен';$_['entry_status']='Статус';$_['entry_comment']='Вътрешна бележка';$_['text_files']='Файлове';$_['text_history']='История';
+$_['button_filter']='Филтър';$_['button_delete']='Изтрий';$_['button_view']='Преглед';$_['button_save']='Запази';$_['button_cancel']='Назад';
+$_['status_new']='Ново';$_['status_viewed']='Прегледано';$_['status_needs_info']='Изисква информация';$_['status_quote_sent']='Изпратена оферта';$_['status_in_progress']='В процес';$_['status_accepted']='Прието';$_['status_rejected']='Отказано';$_['status_completed']='Приключено';$_['status_spam']='Спам';
+$_['error_permission']='Нямате права за промяна на запитванията.';
