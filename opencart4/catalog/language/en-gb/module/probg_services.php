8fa39ad7ab1f3ecce867c9a80c51e7d73295b59b
Veselin Totev <v.totev@probg.com>
2026-08-26 13:11:39 +0300

    docs(oc4): expand English storefront language

--- opencart4/catalog/language/en-gb/module/probg_services.php
+++ opencart4/catalog/language/en-gb/module/probg_services.php
@@ -1,3 +1,6 @@
 <?php
 $_['heading_title'] = 'Services';
-$_['text_stage'] = 'The OpenCart 4 edition of ProBG Services is under active development.';
+$_['text_categories'] = 'Categories';
+$_['text_services'] = 'Latest services';
+$_['text_empty'] = 'No services are available yet.';
+$_['text_read_more'] = 'Learn more';
