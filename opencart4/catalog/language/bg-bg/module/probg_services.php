464a86c6b38422c1fd086e761631bd9c77f3f461
Veselin Totev <v.totev@probg.com>
2026-08-26 13:11:56 +0300

    docs(oc4): expand Bulgarian storefront language

--- opencart4/catalog/language/bg-bg/module/probg_services.php
+++ opencart4/catalog/language/bg-bg/module/probg_services.php
@@ -1,3 +1,6 @@
 <?php
 $_['heading_title'] = 'Услуги';
-$_['text_stage'] = 'OpenCart 4 версията на ProBG Services е в активна разработка.';
+$_['text_categories'] = 'Категории';
+$_['text_services'] = 'Последни услуги';
+$_['text_empty'] = 'Все още няма публикувани услуги.';
+$_['text_read_more'] = 'Научи повече';
