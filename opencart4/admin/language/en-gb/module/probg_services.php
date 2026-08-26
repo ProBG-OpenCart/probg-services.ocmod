0eca036045f74a4187dce7e04de7de6074584bf1
Veselin Totev <v.totev@probg.com>
2026-08-26 11:46:31 +0300

    feat(oc4): add English administration language

---
+++ opencart4/admin/language/en-gb/module/probg_services.php
@@ -0,0 +1,6 @@
+<?php
+$_['heading_title'] = 'ProBG Services';
+$_['text_stage'] = 'OpenCart 4 development';
+$_['text_version'] = 'Version';
+$_['text_stage_label'] = 'Stage';
+$_['text_info'] = 'The separate OpenCart 4 implementation has started with its own package contract and namespaced integration layer.';
