f0a0c4fb04bc1f682c1687904185b3202723e8d5
Veselin Totev <v.totev@probg.com>
2026-08-26 10:16:04 +0300

    feat: add enquiry rate-limit query

--- upload/catalog/model/extension/probg_services/enquiry.php
+++ upload/catalog/model/extension/probg_services/enquiry.php
@@ -2,21 +2,10 @@
 class ModelExtensionProbgServicesEnquiry extends Model {
     public function addEnquiry($data) {
         $this->db->query("INSERT INTO `" . DB_PREFIX . "probg_service_enquiry` SET store_id='" . (int)$this->config->get('config_store_id') . "', service_id='" . (int)$data['service_id'] . "', customer_id='" . (int)$data['customer_id'] . "', status_id='1', assigned_user_id='0', name='" . $this->db->escape($data['name']) . "', email='" . $this->db->escape($data['email']) . "', telephone='" . $this->db->escape($data['telephone']) . "', company='" . $this->db->escape($data['company']) . "', website='" . $this->db->escape($data['website']) . "', subject='" . $this->db->escape($data['subject']) . "', message='" . $this->db->escape($data['message']) . "', budget='" . $this->db->escape($data['budget']) . "', desired_deadline='" . $this->db->escape($data['desired_deadline']) . "', ip='" . $this->db->escape($data['ip']) . "', user_agent='" . $this->db->escape($data['user_agent']) . "', email_sent='0', date_added=NOW(), date_modified=NOW()");
-
-        $enquiry_id = (int)$this->db->getLastId();
-        $this->addHistory($enquiry_id, 1, 'Enquiry created from storefront.', 0);
-        return $enquiry_id;
+        $enquiry_id=(int)$this->db->getLastId();$this->addHistory($enquiry_id,1,'Enquiry created from storefront.',0);return $enquiry_id;
     }
-
-    public function addFile($enquiry_id, $data) {
-        $this->db->query("INSERT INTO `" . DB_PREFIX . "probg_service_enquiry_file` SET enquiry_id='" . (int)$enquiry_id . "', name='" . $this->db->escape($data['name']) . "', filename='" . $this->db->escape($data['filename']) . "', mime_type='" . $this->db->escape($data['mime_type']) . "', size='" . (int)$data['size'] . "', date_added=NOW()");
-    }
-
-    public function markEmailSent($enquiry_id) {
-        $this->db->query("UPDATE `" . DB_PREFIX . "probg_service_enquiry` SET email_sent='1', date_modified=NOW() WHERE enquiry_id='" . (int)$enquiry_id . "'");
-    }
-
-    public function addHistory($enquiry_id, $status_id, $comment, $notify = 0) {
-        $this->db->query("INSERT INTO `" . DB_PREFIX . "probg_service_enquiry_history` SET enquiry_id='" . (int)$enquiry_id . "', status_id='" . (int)$status_id . "', user_id='0', comment='" . $this->db->escape($comment) . "', notify='" . (int)$notify . "', date_added=NOW()");
-    }
+    public function addFile($enquiry_id,$data){$this->db->query("INSERT INTO `".DB_PREFIX."probg_service_enquiry_file` SET enquiry_id='".(int)$enquiry_id."', name='".$this->db->escape($data['name'])."', filename='".$this->db->escape($data['filename'])."', mime_type='".$this->db->escape($data['mime_type'])."', size='".(int)$data['size']."', date_added=NOW()");}
+    public function markEmailSent($enquiry_id){$this->db->query("UPDATE `".DB_PREFIX."probg_service_enquiry` SET email_sent='1', date_modified=NOW() WHERE enquiry_id='".(int)$enquiry_id."'");}
+    public function addHistory($enquiry_id,$status_id,$comment,$notify=0){$this->db->query("INSERT INTO `".DB_PREFIX."probg_service_enquiry_history` SET enquiry_id='".(int)$enquiry_id."',status_id='".(int)$status_id."',user_id='0',comment='".$this->db->escape($comment)."',notify='".(int)$notify."',date_added=NOW()");}
+    public function getRecentCountByIp($ip,$minutes){$minutes=max(1,min(1440,(int)$minutes));$q=$this->db->query("SELECT COUNT(*) AS total FROM `".DB_PREFIX."probg_service_enquiry` WHERE store_id='".(int)$this->config->get('config_store_id')."' AND ip='".$this->db->escape($ip)."' AND date_added >= DATE_SUB(NOW(),INTERVAL ".$minutes." MINUTE)");return (int)$q->row['total'];}
 }
