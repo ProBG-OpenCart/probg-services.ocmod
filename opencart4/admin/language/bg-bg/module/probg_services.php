8553dbc5153c7ba19e91526a273aa43c0dbaaab7
Veselin Totev <v.totev@probg.com>
2026-08-26 11:46:22 +0300

    feat(oc4): add Bulgarian administration language

---
+++ opencart4/admin/language/bg-bg/module/probg_services.php
@@ -0,0 +1,6 @@
+<?php
+$_['heading_title'] = 'ProBG Services';
+$_['text_stage'] = 'OpenCart 4 разработка';
+$_['text_version'] = 'Версия';
+$_['text_stage_label'] = 'Етап';
+$_['text_info'] = 'Започната е отделната OpenCart 4 реализация със собствен package contract и namespaced integration layer.';
